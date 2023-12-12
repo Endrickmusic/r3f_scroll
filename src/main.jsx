0ffc5e5ef45ace9057383c9c4720d31df2d92c46
Christian Hohenbild <mail@christianhohenbild.de>
2023-12-12 02:00:40 +0100

    initial setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client'
+import { Logo } from '@pmndrs/branding'
 import './index.css'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+function Overlay() {
+  return (
+    <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
+      <a href="https://pmnd.rs/" style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px' }}>
+        pmnd.rs
+        <br />
+        dev collective
+      </a>
+      <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>good —</div>
+      <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>08/01/2023</div>
+    </div>
+  )
+}
+
+createRoot(document.getElementById('root')).render(
+  <>
     <App />
-  </React.StrictMode>,
+    <Overlay />
+    <Logo style={{ position: 'absolute', bottom: 40, left: 40, width: 30 }} />
+  </>
 )
